66693cf3e67a0d0ec9f712102f925c2106df8f7b
Sunil Kumar <77500332+sunil-hash706@users.noreply.github.com>
2021-09-27 19:24:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const tm1 = document.getElementById('tm1');
+const tm2 = document.getElementById('tm2');
+const tm3 = document.getElementById('tm3');
+const tm4 = document.getElementById('tm4');
+
+fn1=()=>{
+    tm1.classList.add("teammembero");
+}
+fn2=()=>{
+    tm2.classList.add("teammembero");
+}
+fn3=()=>{
+    tm3.classList.add("teammembero");
+}
+fn4=()=>{
+    tm4.classList.add("teammembero");
+}
+fno1=()=>{
+    tm1.classList.remove("teammembero");
+}
+fno2=()=>{
+    tm2.classList.remove("teammembero");
+}
+fno3=()=>{
+    tm3.classList.remove("teammembero");
+}
+fno4=()=>{
+    tm4.classList.remove("teammembero");
+}
